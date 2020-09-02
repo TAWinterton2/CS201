387d0d92371b84b132580b4edfe3f40d8d2fdd4e
Travis Winterton <tawinterton2@alaska.edu>
2020-09-02 00:06:08 -0800

    Created new solution for part 2 of homework 0

---
+++ HW/HW0-2/HW0-2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+/*
+TRAVIS WINTERTON
+CS201
+WEEK 08/31/20
+
+HOMEWORK 0 PART 2
+
+PURPOSE OF PROGRAM
+	Print out poem
+*/
+
+int main() {
+	cout << "All that is gold does not glitter," << '\n';
+	cout << "Not all those who wander are lost;" << '\n';
+	cout << "THe old that is strong does not wither," << '\n';
+	cout << "Deep roots are not reached by the frost." << '\n';
+	cout << "From the ashes a fire shall be woken," << '\n';
+	cout << "A light from the shadows shall spring;" << '\n';
+	cout << "Renewed shall be blade that was broken," << '\n';
+	cout << "The crownless agian shall be kin." << '\n';
+	while (cin.get() != '\n')
+		return 0;
+}
