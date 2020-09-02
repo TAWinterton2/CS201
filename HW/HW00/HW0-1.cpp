cf46c458030852c7d8af57ef894cee5b28790bcd
Travis Winterton <tawinterton2@alaska.edu>
2020-09-01 23:41:12 -0800

    Changed names

---
+++ HW/HW00/HW0-1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+/*
+* TRAVIS WINTERTON
+* CS210
+* WEEK 08/31/20
+* HOMEWORK 00
+* PURPOSE OF PROGRAM
+*
+* PART 1:
+*	Prints statement "Hello, World!" five times
+*/
+int main() {
+	int i;
+	// part one of program
+	for (i = 0; i <= 6; i++)
+	{
+		cout << "Hello, World!!!" << '\n';
+	};
+	cout << "Press ENTER to Continue" << endl;
+	while (cin.get() != '\n')
+		return 0;
+}
+
