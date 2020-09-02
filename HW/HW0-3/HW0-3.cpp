f58405903f9f54e5c8c25076e8032bedab282953
Travis Winterton <tawinterton2@alaska.edu>
2020-09-02 00:35:25 -0800

    Added part 3 of Homework 0

---
+++ HW/HW0-3/HW0-3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+/*
+TRAVIS WINTERTON
+CS201
+WEEK 08/31/20
+PURPOSE OF PROGRAM
+	Write a description of your favorite game
+
+*/
+
+int main() {
+	cout << "Based on the popluar Tabletop RPG" << '\n';
+	cout << "In this Action RPG, You are plunged into the dark gritty world of " << '\n';
+	cout << "modern Los Angales and it's vampire underworld.  " << '\n';
+	cout << "You are inducted into this society agiants your will and now must work your way from " << '\n';
+	cout << "a fledgling, and earn respect from the highest seats of vampire society" << '\n';
+	cout << "as you journery into the dark heart of the city, the choices you make will" << '\n';
+	cout << "decided the fate who maintain control over of Los Angeles " << '\n';
+	return 0;
+
+}
