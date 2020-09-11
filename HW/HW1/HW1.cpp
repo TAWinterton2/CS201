6dd44c905f0fe35527f510ed3d3ab05acb70ff97
Travis Winterton <tawinterton2@alaska.edu>
2020-09-10 20:17:56 -0800

    Created new project for HW1 - Program can accept integer from user and recongize user input error - Added for loop that prints out correct amount of lines for diamond

---
+++ HW/HW1/HW1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+/*
+TRAVIS WINTERTON
+HW 1
+CS 201
+WEEK OF 09/07/20
+
+PURPOSE OF PROGRAM
+	PROGRAM PROMTS USER FOR AN INTEGER FORM THE USER USING LINE-ORIENTED INPUT WITH PROPER ERROR HANDLING\
+
+	PROGRAM THEN PRINTS A CORRESPONDINGLY SIZED DIAMOND MADE OF POUND (#) SGNS.
+
+	EXAMPLE:
+		IF USER INPUTS 3, THEN PROGRAM SHOULD PRINT OUT:
+							#
+						   ###
+						  #####
+						   ###
+						    #
+		SIMILARLY, IF THE USER ENTERS 7, THE PROGRAM SHOULD PRINT THE FOLLOWING:
+							#
+						   ###
+						  #####
+						#########
+					   ###########
+					  #############
+					 ###############
+					  #############
+					   ###########
+					    #########
+						  #####
+						   ###
+							#
+*/			
+
+
+int main() {
+	//variable list
+	int user_input;
+	int k;
+	
+	do {
+		cout << "Please enter a  postive integer: " << '\n';
+			cin >> user_input;
+			if (user_input < 0) {
+				cout << user_input << " Is not a postive integer" << endl;
+		}
+	} while (user_input < 0);
+	for (k = 1; k <= user_input * 2 - 1; k++) {
+		cout << "bruh" << endl;
+
+	}
+	return 0;
+}
