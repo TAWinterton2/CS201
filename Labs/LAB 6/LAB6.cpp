173569da51371905ad551bc6fb7c263ac592b3af
Travis Winterton <tawinterton2@alaska.edu>
2020-09-14 10:26:17 -0800

    Finisehd lab six program can get string input form user and display string length, # = to string length, and display user_input reveresed.

--- Labs/LAB 6/LAB6.cpp
+++ Labs/LAB 6/LAB6.cpp
@@ -2,7 +2,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
-
+using std::string;
 /*
 TRAVIS WINTERTON
 CS201
@@ -12,17 +12,31 @@ PURPOSE OF PROGRAM:
 	FUNCTION TAKES IN STRING PARAMETER. FUNCTION SHOULD PRINT THE STRING AND ITS LENGTH IN SOME NEAT, UNDERSTANDABLE MANNER.
 
 */
+//variable list
+string user_input;
 
 //FUNCTIONS
-void String_Reader(std::string user_input) {
-	//get user input
+void String_Reader(string user_input) {
+	int string_length;
 	//get lenght of string statemetn
+	string_length = user_input.length();
 	//print out string and string lenght
+	cout << user_input << endl;
+	cout << "String length: " << string_length << endl;
+	// print out the character "#" equal to the length of the user_input
+	for (int i = 0; i <= string_length; i++) {
+		cout << "#";
+	}
+	cout << '\n';
+	// reverse string
+	reverse(user_input.begin(), user_input.end());
+	cout << user_input << endl;
 }
 
 
 int main() {
-
-	String_Reader(); //call function
+	cout << "Enter a string: ";
+	cin >> user_input;
+	String_Reader(user_input); //call function
 	return 0;
 }
