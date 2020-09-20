0875d630b65f2a5d9214603f807437b0337ccbe9
Travis Winterton <tawinterton2@alaska.edu>
2020-09-19 21:09:28 -0800

    Added function "InputNames"; Stores names from user into vector

--- HW/HW 2-1/HW2-1.cpp
+++ HW/HW 2-1/HW2-1.cpp
@@ -27,13 +27,24 @@ using std::endl;
 using std::string;
 using std::vector;
 
+//global Variables
+string names;
+vector <string> names_list,
+
+//Function list
+void InputNames(string names) {
+	names_list.push_back(names);
+}
+
+
 int main() {
-	string name;
-	vector <string> names_list;
-	for (int i = 0; i <= 10; ++i) {
-		cout << "Enter a Name";
-		getline(cin, name);
-		names_list.push_back(name);
+	for (int i = 0; i < 10; ++i) {
+		cout << "Enter a Name: ";
+		getline(cin, names);
+		InputNames(names);
 	}
 	return 0;
 }
+
+
+
