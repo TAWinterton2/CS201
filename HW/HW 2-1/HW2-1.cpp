843a3ca43a2a6119d21bcb49b7ae6d19340c77eb
Travis Winterton <tawinterton2@alaska.edu>
2020-09-19 20:31:31 -0800

    Created new program for Homework 2, program 1

---
+++ HW/HW 2-1/HW2-1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+
+/*
+TRAVIS WINTERTON
+CS201
+HW 2 - 1 (MAIN PROGRAM)
+WEEK OF 09/14/20
+PURPOSE OF PROGARM
+	Write a progarm that uses std::vector class w/ a variety of loops. 
+	Write and test a function that encapsulats the code in the main() function.
+	EX : InputNames(std::vector<std::styring> & names);
+
+	Write and test a function that searches for a name in the list. Return a bool value whether 
+	the name exist or not. Use the function prototype.
+
+	Write a function that  takes the list of names and prints them out. use the function prototype
+
+*/
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+	
+
+}
