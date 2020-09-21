84e2a62e3d27d33d432a6fe1a66be039dc28945c
Travis Winterton <tawinterton2@alaska.edu>
2020-09-21 09:40:39 -0800

    Lab 7 completed

--- Labs/LAB 7/Lab7.cpp
+++ Labs/LAB 7/Lab7.cpp
@@ -2,6 +2,15 @@
 #include<iostream>
 
 
+/*
+	TRAVIS WINTERTON
+	CS 201
+	WEEK OF 09/21/20
+
+	PURPOSE OF PROGRAM:
+		Write a program that inputs 3 integers using 3 calls to getint. The program should then print the 3 integers in an aligned column.
+*/
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -9,21 +18,24 @@ using std::endl;
 
 
 int main() {
-	int x, y, z;
+	int x, y, z, space;
 
-	cout << "Enter in a number: " << '\n';
-	cin >> x;
+	cout << "Enter in a integer: " << '\n';
 	x = getInt();
 	cout << "Enter in another one: " << '\n';
-	cin >> y;
 	y = getInt();
 	cout << "One more time: " << '\n';
-	cin >> z;
 	z = getInt();
 
-	cout << putInt() << endl;
-	cout << y << endl;
-	cout << z << endl;
+	cout << "enter in # of spaces to make: ";
+	cin >> space;
+	
+	putInt(x, space);
+	cout << endl;
+	putInt(y, space);
+	cout << endl;
+	putInt(z, space);
+	cout << endl;
 	return 0;
 }
 
