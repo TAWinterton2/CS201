22f50b66d3299482d669f563192d1ec88016e7fa
Travis Winterton <tawinterton2@alaska.edu>
2020-09-21 19:55:12 -0800

    Completed first draft of HW 2. program can ask user for # of differnt coins, and calculate total value

--- HW/HW 2-2/HW 2-2.cpp
+++ HW/HW 2-2/HW 2-2.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<math.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
 
 
 
@@ -22,7 +28,46 @@ PURPOSE OF PROGRAM
 				value of all coins: 573 cents
 */
 
+//calculates total value of all coins
+void coin_amount_calc(int & pennies, int & nickles, int & dimes, int & quarters, int & dollars) {
+	double penny_value = 0.1;
+	double nickle_value = 0.5;
+	double dime_value = 0.10;
+	double quater_value = .25;
+	double dollar_value = 1.00;
 
+	const double Total_amount = (pennies * penny_value) + (nickles * nickles) + (dimes * dimes) + (quarters * quater_value) + (dollars * dollars);
+	
+	cout << "Your total value of all coins is: " << Total_amount;
+
+}
+
+// main collects # of coins of each type from user
 int main() {
+	//list of values associated with each coic
+	
+	int penny_amount;
+	int nickle_amount;
+	int dime_amount;
+	int quater_amount;
+	int dollar_amount;
+
+	cout << "Enter amount of Pennies: ";
+	cin >> penny_amount;
+
+	cout << "Enter amount of Nickles: ";
+	cin >> nickle_amount;;
+
+	cout << "Enter amount of Dimes: ";
+	cin >> dime_amount;
+
+	cout << "Enter amount of Quarters ";
+	cin >> quater_amount;
+
+	cout << "Enter amount of Dollars: ";
+	cin >> dollar_amount;
+
+	return 0;
+
 
 }
