9e2fca1e242291c421c7de143896c4892d43022d
Travis Winterton <tawinterton2@alaska.edu>
2020-09-21 19:26:51 -0800

    Created home work 2 program 2

---
+++ HW/HW 2-2/HW 2-2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+
+/*
+TRAVIS WINTERTON
+CS 201
+HW 2 (PROGRAM 2)
+
+WEEK OF 09/ 21/ 20
+
+PURPOSE OF PROGRAM
+	Prompt user to enter # of pennies, nickles, dimes, quarters, and one-dollar coins. Query the user seperatly
+	for the number of each size coin. (E.G "How many pennies do you have?") than the program should print out somthing like this:
+				You Have 23 pennies.
+				You have 17 nickles.
+				you have 14 dimes.
+				you have 7 quarters.
+				you have 3 half dollars
+				value of all coins: 573 cents
+*/
+
+
+int main() {
+
+}
