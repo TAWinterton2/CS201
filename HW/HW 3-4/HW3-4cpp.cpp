44d06dde696ee62cd7a803c4294d0dfbd45e35b6
Travis Winterton <tawinterton2@alaska.edu>
2020-09-30 18:34:03 -0800

    HW 3-4 created, main cpp and header file for function QuadraticComp created

---
+++ HW/HW 3-4/HW3-4cpp.cpp
@@ -0,0 +1,27 @@
+/*
+Travis Winterton
+CS201
+HW3-4
+Week of 09/28/20
+
+Purpose of Program
+	Write a program that can calcuate x for a quadratic equation. 
+	Create a function that prints out the roots of a quadratic euqation, given a,b,c
+	When the program detects an equation with no real roots, have it print out a message.
+
+*/
+
+
+#include<math.h>
+#include<iostream>
+#include<string>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+
+}
---
+++ HW/HW 3-4/Quadratic.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#ifndef FILE_QUADRATIC_HPP
+#define FILE_QUADRATIC_HPP
+
+//QudraticComp
+//ask user to input numbers of a quadratic equation
+//function then computes both roots 
+//if no real roots can be given, function prints message regarding it. 
+#endif
