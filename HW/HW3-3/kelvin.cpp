e57df8bd1a94e392c9f595fa2be939805caf7345
Travis Winterton <tawinterton2@alaska.edu>
2020-09-30 18:06:31 -0800

    added in revised code for kelvin.cpp

--- HW/HW3-3/kelvin.cpp
+++ HW/HW3-3/kelvin.cpp
@@ -17,13 +17,14 @@
 //Converts Celsisus to Kelvin
 
 
-//program has no header files 
+
+
+/*
+//program has no header files
 //program cannot use cout or cin w/out #include<iostream> and using std::cout || using std::cin
 
-
 double ctok(double c)
-{	
-	//converions rate is correct
+{
 	int k = c + 273.15;
 	//function returns a data type of "int" , no value will actually be returned
 	//suggested edit: have the fucntion return the value of k
@@ -37,11 +38,39 @@ int main() {
 	//suggested edit: Either have the program start with a predetermin value to convert to kelvin or have program prompt user for value to convert
 	cin >> d;
 
-	//program is trying to call function "ctok" but parameter does not take a char
+	//program is trying to call function "ctok" but for function "ctoK", parameter does not take a char 
+	//suggested edit: change parameter so it passes the correct data type on double c
 	double k = ctok("c");
 	//C in cout is captialized, where it should be all lower case
 	//line is trying to end with insterting a new line, it should be '\n' not '/n'
 	Cout << k << '/n';
 
 	//no retrun 0;
+}
+
+*/
+
+//kelvin code reviesed
+
+#include<iostream>
+#include<math.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+double ctok(double c) {
+	int k = c + 273.15;
+	return k;
+}
+
+int main() {
+	double c;
+	cout << "Enter in celsius amount to convert to kelvin: ";
+	cin >> c;
+
+	double k = ctok(c);
+	cout << k << '\n';
+	return 0;
 }
