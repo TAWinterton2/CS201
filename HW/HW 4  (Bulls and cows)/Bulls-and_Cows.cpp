6fea027e1ee052a2859f8528e7183055f27fbe29
Travis Winterton <tawinterton2@alaska.edu>
2020-10-09 22:29:35 -0800

    HW Bulls and cows created

---
+++ HW/HW 4  (Bulls and cows)/Bulls-and_Cows.cpp
@@ -0,0 +1,47 @@
+#pragma once
+
+//Travis Winterton
+//HW 4 - 2
+//Week of 09/28/20
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+int main() {
+	int bull;
+	int cow;
+	int guess;
+
+	vector<int> bullandcow{
+		1,7,9,9
+	};
+	int n = bullandcow.size();
+	vector<int> user_guess(n);
+
+	cout << "Try to Guess the Numbers! (0 - 9): ";
+	cout << "Enter in a negative number to show answer";
+	cin >> guess;
+	for (auto B : user_guess) {
+		cin >> guess;
+		if (guess < 0) {
+			for (auto a : bullandcow) {
+				cout << a << endl;
+				break;
+			}
+		}
+
+	}
+
+
+	
+	return 0;
+}
