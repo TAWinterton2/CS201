8f7180488acb433ecd5f92f7bbaf3ba3b9a39c37
Travis Winterton <tawinterton2@alaska.edu>
2020-10-09 16:29:54 -0800

    main() function created

---
+++ HW/HW 4 - 2/crud.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+#include<utility>
+#include<algorithm>
+#include<map>
+#include"Database.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::pair;
+using std::vector;
+
+
+int main() {
+	cout << "Enter in an option";
+}
