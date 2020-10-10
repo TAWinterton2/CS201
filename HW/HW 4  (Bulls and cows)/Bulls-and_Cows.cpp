cfc46df778d1458aef4243a2af2962613532e8f5
Travis Winterton <tawinterton2@alaska.edu>
2020-10-09 22:39:15 -0800

    updated code for chekcin user inputed an negative integer to display answer

--- HW/HW 4  (Bulls and cows)/Bulls-and_Cows.cpp
+++ HW/HW 4  (Bulls and cows)/Bulls-and_Cows.cpp
@@ -17,9 +17,9 @@ using std::vector;
 
 
 int main() {
-	int bull;
-	int cow;
-	int guess;
+	//int bull;
+	//int cow;
+	//int guess;
 
 	vector<int> bullandcow{
 		1,7,9,9
@@ -28,13 +28,20 @@ int main() {
 	vector<int> user_guess(n);
 
 	cout << "Try to Guess the Numbers! (0 - 9): ";
-	cout << "Enter in a negative number to show answer";
-	cin >> guess;
-	for (auto B : user_guess) {
-
+	cout << "Enter in a negative number to show answer: " << endl;
+	for (int a = 0; a < n; a++) {
+		cin >> user_guess[a];
+		//if user input is less than 0, display answer and end program
+		if (user_guess[a] < 0) {
+			for (auto p : bullandcow) 
+				cout << p << endl;
+				return 0;
+			
+		}
 	}
-
-
+	for (auto a : user_guess) {
+		cout << a << endl;
+	}
 	
 	return 0;
 }
