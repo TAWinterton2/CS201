dc8432cd15ec031cea8ea52d8336cbdbd5b85015
Travis Winterton <tawinterton2@alaska.edu>
2020-10-09 16:27:54 -0800

    updated code for bool CreateRecord() to getlp title and score

--- HW/HW 4 - 2/Database.cpp
+++ HW/HW 4 - 2/Database.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include<utility>
 #include<algorithm>
+#include<map>
 #include"Database.h"
 
 using std::cout;
@@ -14,10 +15,16 @@ using std::pair;
 using std::vector;
 
 
+std::map<string, Music_ratings> Database;
+
 //create a new data set
-bool CreateRecord(const string& key) {
+bool CreateRecord(const string& key, float &score) {
+	Music_ratings LP_name;
+	Music_ratings user_score;
 	string LP_title;
+	float user_score;
 	cout << "Enter in Title of LP: ";
 	getline(cin, LP_title);
-	LP_title.title;
+	LP_name.title = LP_title;
+
 }
