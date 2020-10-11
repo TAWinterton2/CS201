3e9c3e50322d743a3a2d7509044c73aede6e0860
Travis Winterton <tawinterton2@alaska.edu>
2020-10-10 22:00:01 -0800

    HW 4(FIFO_LIFO) created w/ 3 files

---
+++ HW/HW4(FIFO_LIFO)/functions.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+#ifndef FILE_FUNCTIONS_HPP
+#define FILE_FUNCTIONS_HPP
+
+#endif
---
+++ HW/HW4(FIFO_LIFO)/functions.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+#pragma once
