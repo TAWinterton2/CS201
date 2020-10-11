d2689ea3e3ff3f2c598a1f52f1896c36a302d75d
Travis Winterton <tawinterton2@alaska.edu>
2020-10-10 22:07:08 -0800

    functions defined in functions.hpp

--- HW/HW4(FIFO_LIFO)/functions.h
+++ HW/HW4(FIFO_LIFO)/functions.h
@@ -11,3 +11,19 @@ using std::endl;
 using std::string;
 using std::vector;
 #pragma once
+
+
+//Functions for First-in First-out
+//array pop in 
+void FifoPush(vector<string>& container, const string& item);
+//array pop out
+void FifoPop(vector<string>& container, const string& item);
+
+//Functions for Last-in Fisrt out
+void LifoPush(vector<string>& container, const string& item);
+void LifoPop(vector<string>& container, const string& item);
+
+
+//shared functionality
+bool IsContainerEmpy(const vector<string>& containter);
+void Printcontainer(const vector<string>& containter);
