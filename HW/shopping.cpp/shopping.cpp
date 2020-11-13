8c1fa8317993a7189f5d2db7520a2ad5829993a5
Travis Winterton <tawinterton2@alaska.edu>
2020-11-12 22:01:35 -0900

    added menu for user to naviagte options in program

--- HW/shopping.cpp/shopping.cpp
+++ HW/shopping.cpp/shopping.cpp
@@ -14,5 +14,18 @@ using std::string;
 using std::vector;
 
 int main() {
+	int menu_choice;
+	std::map<string, int> cart;
 
+	//main menu
+	while (menu_choice != 4) {
+		cout << "Welcome to KOPINI Store front!" << '\n';
+		cout << "What would you like to do?" << '\n';
+		cout << "1) Add Item" << '\n';
+		cout << "2) Remove Item" << '\n';
+		cout << "3) Look at cart" << '\n';
+		cout << "4) Check out" << '\n';
+		cin >> menu_choice;
+	}
+	
 }
