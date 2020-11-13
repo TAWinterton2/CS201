b262770aa20c50cce1c2419c3599b3bd73316e97
Travis Winterton <tawinterton2@alaska.edu>
2020-11-12 23:10:10 -0900

    Menu fully works

--- HW/shopping.cpp/shopping.cpp
+++ HW/shopping.cpp/shopping.cpp
@@ -17,16 +17,41 @@ using std::vector;
 int main() {
 	int menu_choice = 0;
 	std::map<string, int> cart;
-
+	vector<string> stock;
 	//main menu
+	cout << "Welcome to KOPINI Store front!" << '\n';
 	while (menu_choice != 4) {
-		cout << "Welcome to KOPINI Store front!" << '\n';
 		cout << "What would you like to do?" << '\n';
 		cout << "1) Add Item" << '\n';
 		cout << "2) Remove Item" << '\n';
 		cout << "3) Look at cart" << '\n';
 		cout << "4) Check out" << '\n';
+
 		cin >> menu_choice;
+		switch (menu_choice) {
+			case 1:
+				AddItem(cart, stock);
+				cout << endl;
+				continue;
+			case 2:
+				RemoveItem(cart);
+				cout << endl;
+				continue;
+			case 3:
+				cout << "placeholder";
+				cout << endl;
+				continue;
+			case 4:
+				Checkout(cart);
+				cout << endl;
+				break;
+			default:
+				cout << "Option not valid" << '\n';
+				continue;
+
+		}
+			
+
 	}
-	
+	return 0;
 }
