2038f55bb040abc355ed83d3afa74b10bd10ebf4
Travis Winterton <tawinterton2@alaska.edu>
2020-11-14 02:33:33 -0900

    test case for inner_product() succesfful

--- HW/math-catch/catch-math.cpp
+++ HW/math-catch/catch-math.cpp
@@ -23,11 +23,24 @@ TEST_CASE("Show that atan2 computes given value properly", "atan2(-10.0, 10.0) =
 }
 
 //test case for accumulate()
-TEST_CASE("Show that Accumulate() computes given values properly", "Accumulate(v.begin(), v.end(), 0) == 55") {
+TEST_CASE("Show that Accumulate() computes properly with given values", "Accumulate(v.begin(), v.end(), 0) == 55") {
 	std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 	int sum = std::accumulate(v.begin(), v.end(), 0);
 
 	REQUIRE(sum == 55);
+}
+
+//test case for inner_product()
+TEST_CASE("SHow that inner_product() computes properly with given values", "Inner_product() == 240")
+{
+	int init = 100;
+	int v1[] = { 10,20,30 };
+	int v2[] = { 1,2,3 };
+
+	int sum = std::inner_product(v1, v1 + 3, v2, init);
+	
+	REQUIRE(sum == 240);
+
 }
 
